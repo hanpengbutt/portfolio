b1c77d66b2e598779a054c0f085dbe4cea1efb57
한지은 <hjn510450@gmail.com>
2026-05-03 14:39:57 +0900

    💄 Style: 공통, blog 라우트 레이아웃 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="border-border-default text-text-primary fixed flex h-25 w-full items-center justify-between border-b border-solid bg-white px-15">
+    <header className="border-border-default text-text-primary fixed top-0 left-0 z-50 flex h-25 w-full items-center justify-between border-b border-solid bg-white px-15">
       <Link href="/" className="text-brand-logo">
-        pengbutt
+        pengbutt*
       </Link>
       <nav className="text-body-md flex items-center gap-15">
         <Link href="/about">ABOUT</Link>
