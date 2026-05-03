8d24ea2a7e4100f11432eff0ed894b0b885bf52b
한지은 <hjn510450@gmail.com>
2026-05-03 15:27:06 +0900

    🐛 Fix: 빌드 과정에서 @import 순서로 인해 발생하는 에러 해결(<link>에서 폰트 가져오도록 수정)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,12 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css"
+        />
+      </head>
       <body className="flex min-h-full flex-col bg-white pt-25">
         <Header />
         {children}
