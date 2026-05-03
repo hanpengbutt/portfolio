629c5b8c83be39692bcfba327052422d6d15c653
한지은 <hjn510450@gmail.com>
2026-05-03 13:57:48 +0900

    ✨ Feat: blog 라우트의 그리드 레이아웃 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex h-25 w-full items-center justify-between border-b border-solid border-border-default px-15 text-text-primary">
+    <header className="border-border-default text-text-primary fixed flex h-25 w-full items-center justify-between border-b border-solid bg-white px-15">
       <Link href="/" className="text-brand-logo">
         pengbutt
       </Link>
-      <nav className="flex items-center gap-15 text-body-md">
+      <nav className="text-body-md flex items-center gap-15">
         <Link href="/about">ABOUT</Link>
         <Link href="/blog">BLOG</Link>
         <Link href="/projects">PROJECTS</Link>
