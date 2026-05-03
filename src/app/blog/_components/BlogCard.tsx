2631b4ecd0981b5b77d3b983bea670cdedcd835c
한지은 <hjn510450@gmail.com>
2026-05-03 14:42:59 +0900

    ✨ Feat: 게시글 목록을 나타내는 BlogCard 컴포넌트 구현

---
+++ src/app/blog/_components/BlogCard.tsx
@@ -0,0 +1,49 @@
+import { cn } from "@/utils/cn";
+import Tag from "./Tag";
+import { formatDate } from "@/utils/date";
+
+export interface BlogCardProps {
+  title: string;
+  description: string;
+  tag: string;
+  date: Date | string;
+  className?: string;
+}
+
+export default function BlogCard({
+  title,
+  description,
+  tag,
+  date,
+  className = "",
+}: BlogCardProps) {
+  return (
+    <div
+      className={cn(
+        "border-border-default flex h-80 w-full max-w-90 flex-col items-start justify-between overflow-hidden border border-solid bg-white px-5 py-4",
+        className,
+      )}
+    >
+      {/* Image Placeholder */}
+      <div className="border-border-default h-35 w-full shrink-0 border border-solid" />
+
+      {/* Title */}
+      <h3 className="text-heading-md text-text-primary w-full truncate">
+        {title}
+      </h3>
+
+      {/* Description */}
+      <p className="text-body-sm text-text-primary line-clamp-2 w-full">
+        {description}
+      </p>
+
+      {/* Footer */}
+      <div className="flex w-full shrink-0 items-center justify-between">
+        <span className="text-body-sm text-text-primary">
+          {formatDate(date)}
+        </span>
+        <Tag>{tag}</Tag>
+      </div>
+    </div>
+  );
+}
